33e1f1d1053bcb50277357824f0384a2c7cb85a7
Craig Ostrin <craigostrin@gmail.com>
2024-04-08 20:25:28 +0200

    add colors to TW config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          wordle: '#6AA964',
+        },
+        yellow: {
+          wordle: '#C9B458',
+        },
+        gray: {
+          wordle: '#787C7E',
+          light: '#D3D6DA',
+        },
+      },
+    },
   },
   plugins: [],
 }
