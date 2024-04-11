8768e897470ed402f586eb4ea23e6cfd64504ca3
Craig Ostrin <craigostrin@gmail.com>
2024-04-11 14:13:58 +0200

    move isToday to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isToday(date: Date) {
+  const today = new Date().setHours(0, 0, 0, 0)
+
+  return today === date.setHours(0, 0, 0, 0)
+}
