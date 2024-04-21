e470dfc8826509335f37ff195ef8f96a8ef20546
Craig Ostrin <craigostrin@gmail.com>
2024-04-21 20:51:00 +0200

    add date utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,18 @@ export function isToday(date: Date) {
 
   return today === date.setHours(0, 0, 0, 0)
 }
+
+export function daysSince(date: Date): number {
+  const now = new Date()
+  const today = utcFromLocal(now)
+  const diff = today.getTime() - date.getTime()
+  return Math.floor(diff / (1000 * 60 * 60 * 24))
+}
+
+export function utcFromLocal(date: Date): Date {
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  const input = `${year}-${month}-${day}`
+  return new Date(input)
+}
